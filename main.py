8768bbe4f0fd4de4a72ddee5ad99cdaac503846f
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-07-14 19:16:13 -0400

    chore: Update .gitignore and main.py

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import subprocess
+import os
 
 app = FastAPI()
 
-app.title = "Inflex API"
+class CodeRequest(BaseModel):
+    code: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/execute/")
+async def executeCode(request: CodeRequest):
+    code = request.code
+    
+    filePath = 'run.inf'
+    
+    try:
+        with open(filePath, 'w') as file:
+            file.write(code)
+        
+        result = subprocess.run(['python', 'inflex/inFlex.py', filePath], capture_output=True, text=True, check=True)
+        
+        output = result.stdout
+        errors = result.stderr
+        
+    except subprocess.CalledProcessError as e:
+        output = e.stdout
+        errors = e.stderr
+    finally:
+        if os.path.exists(filePath):
+            os.remove(filePath)
+    
+    if errors:
+        raise HTTPException(status_code=400, detail=f"Error executing code: {errors}")
+    
+    return {"result": output}
