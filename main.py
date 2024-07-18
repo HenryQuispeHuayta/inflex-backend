f3ab23a2e1f54deb53be1339a23a2aac15594eaf
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-07-18 01:25:12 -0400

    chore: Add CORS middleware for allowed origins, methods, headers, and credentials

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routes.code import router as code_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
-    app.include_router(code_router, prefix="/api")
+  app = FastAPI()
 
-    return app
+  app.add_middleware(
+    CORSMiddleware,
+    allow_origins = os.getenv("ALLOWED_ORIGINS"),
+    allow_credentials = True if os.getenv("ALLOWED_CREDENTIALS") == "true" else False,
+    allow_methods = os.getenv("ALLOWED_METHODS"),
+    allow_headers = os.getenv("ALLOWED_HEADERS"),
+  )
+  app.include_router(code_router, prefix="/api")
+
+  return app
 
 app = create_app()
